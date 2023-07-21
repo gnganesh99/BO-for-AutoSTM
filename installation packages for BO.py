ecedf5db3490fc44aa006b71cf63f8a0f40ddff0
Ganesh N <gnganesh99@gmail.com>
2023-07-21 15:12:41 -0400

    Add files via upload

---
+++ installation packages for BO.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 15:10:15 2023
+
+@author: ggn
+"""
+
+!pip install --upgrade torch==1.8.1 torchvision==0.9.1
+!pip install gpytorch botorch
+!pip install smt
+
+
+!pip install --upgrade --no-cache-dir gdown
